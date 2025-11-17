52104afe901b8c281a85cfe6d522bcae10ee1d51
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-11-17 22:39:36 +0200

    feat: delete free time

--- server/app/src/main/java/lt/gathertime/server/controller/FreeTimeController.java
+++ server/app/src/main/java/lt/gathertime/server/controller/FreeTimeController.java
@@ -1,6 +1,8 @@
 package lt.gathertime.server.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,11 @@ public class FreeTimeController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createFreeTime(@Valid @RequestBody CreateFreeTimeRequestDTO createFreeTimeRequestDTO) {
         freeTimeService.createFreeTime(createFreeTimeRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFreeTime(@PathVariable Long id) {
+        freeTimeService.deleteFreeTime(id);
     }
 }
