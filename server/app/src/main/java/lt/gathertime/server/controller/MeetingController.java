8bf5de5efc10be408b69a2b80cda1abe75d7b51a
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-11-19 20:12:51 +0200

    add MeetingController with POST /meeting endpoint

---
+++ server/app/src/main/java/lt/gathertime/server/controller/MeetingController.java
@@ -0,0 +1,29 @@
+package lt.gathertime.server.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import lt.gathertime.server.dto.meetingDTOs.CreateMeetingRequestDTO;
+import lt.gathertime.server.service.MeetingService;
+
+@RestController
+@RequestMapping("/meeting")
+@RequiredArgsConstructor
+public class MeetingController {
+
+    private final MeetingService meetingService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createMeeting(@Valid @RequestBody CreateMeetingRequestDTO createMeetingRequestDTO) {
+        meetingService.createMeeting(createMeetingRequestDTO);
+    }
+    
+}
