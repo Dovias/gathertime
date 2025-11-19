02cc7bb6587557e7678bec18c8db823600a8c746
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-11-19 20:09:58 +0200

    add MeetingRepository

---
+++ server/app/src/main/java/lt/gathertime/server/repository/MeetingRepository.java
@@ -0,0 +1,9 @@
+package lt.gathertime.server.repository;
+
+import lt.gathertime.server.model.Meeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+}
