e01669d26348346141c816b59d0eec42951640d6
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-11-25 22:21:37 +0200

    add GET /meeting/user/{userId} endpoint

--- server/app/src/main/java/lt/gathertime/server/controller/MeetingController.java
+++ server/app/src/main/java/lt/gathertime/server/controller/MeetingController.java
@@ -1,11 +1,16 @@
 package lt.gathertime.server.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +18,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import lt.gathertime.server.dto.meetingDTOs.CreateMeetingRequestDTO;
+import lt.gathertime.server.dto.meetingDTOs.MeetingSummaryDTO;
 import lt.gathertime.server.service.MeetingService;
 
 @RestController
@@ -32,5 +38,14 @@ public class MeetingController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void confirmMeeting(@PathVariable Long invitationId) {
         meetingService.confirmMeeting(invitationId);
+    }
+
+    @GetMapping("user/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<MeetingSummaryDTO> getUserMeetings(
+        @PathVariable Long userId, 
+        @RequestParam LocalDateTime startDateTime,  
+        @RequestParam LocalDateTime endDateTime) {
+            return meetingService.getUserMeetings(userId, startDateTime, endDateTime);
     }
 }
