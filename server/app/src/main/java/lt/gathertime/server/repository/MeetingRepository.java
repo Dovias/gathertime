2b127474693bc5c1e28d450e0ac03d0965edb524
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-11-25 22:17:10 +0200

    add findUserMeetingsInRange query

--- server/app/src/main/java/lt/gathertime/server/repository/MeetingRepository.java
+++ server/app/src/main/java/lt/gathertime/server/repository/MeetingRepository.java
@@ -1,9 +1,29 @@
 package lt.gathertime.server.repository;
 
 import lt.gathertime.server.model.Meeting;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+
+    @Query("""
+        SELECT m FROM Meeting m
+        LEFT JOIN m.meetingParticipants p
+        WHERE 
+            p.id = :userId
+            AND m.startDateTime >= :startDateTime
+            AND m.endDateTime <= :endDateTime
+        """)
+    public List<Meeting> findUserMeetingsInRange(
+        @Param("userId") Long userId,
+        @Param("startDateTime") LocalDateTime startDateTime,
+        @Param("endDateTime") LocalDateTime endDateTime
+    );
 }
