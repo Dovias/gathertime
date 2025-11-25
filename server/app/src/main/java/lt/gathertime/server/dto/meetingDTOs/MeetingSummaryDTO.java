363126a2c399593b411c46492ee7b73e06357467
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-11-25 20:21:24 +0200

    add MeetingSummaryDTO

---
+++ server/app/src/main/java/lt/gathertime/server/dto/meetingDTOs/MeetingSummaryDTO.java
@@ -0,0 +1,38 @@
+package lt.gathertime.server.dto.meetingDTOs;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import lt.gathertime.server.dto.userDTOs.UserFullNameDTO;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class MeetingSummaryDTO {
+
+    Long id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @NotNull(message = "Start date and time must not be null")
+    private LocalDateTime startDateTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @NotNull(message = "End date and time must not be null")
+    @Future(message = "End date and time must be in the future")
+    private LocalDateTime endDateTime;
+
+    private String summary;
+
+    private List<Long> meetingActivityIds;
+
+    private List<UserFullNameDTO> participants;
+}
