cb7788654af4fbd21af7ae8aac486648f9fc84af
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-11-26 21:04:31 +0200

    add Friendship entity

---
+++ server/app/src/main/java/lt/gathertime/server/model/Friendship.java
@@ -0,0 +1,40 @@
+package lt.gathertime.server.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Friendship {
+    
+    private Long id;
+
+    private LocalDateTime starDateTime;
+
+    private Boolean isBestFriends;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friend_id")
+    private User friend;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_id")
+    private Chat chat;
+}
