7561c00cdf45bbe4334f41a2d0e5bf5ae148ec10
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-11-26 21:02:46 +0200

    add Chat entity

---
+++ server/app/src/main/java/lt/gathertime/server/model/Chat.java
@@ -0,0 +1,19 @@
+package lt.gathertime.server.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Chat {
+    
+    private Long id;
+}
