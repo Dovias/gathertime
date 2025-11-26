5020d5f1f743ff677402672db2a7b622322492ea
Dovias <70913423+Dovias@users.noreply.github.com>
2025-11-27 01:03:54 +0200

    Add client-side authentication middleware to dashboard pages

--- client/app/pages/dashboard/layout.tsx
+++ client/app/pages/dashboard/layout.tsx
@@ -1,5 +1,36 @@
-import { Outlet } from "react-router";
+import { Outlet, redirect } from "react-router";
 import Navigation from "../../components/main/Navigation";
+import type { Route } from "./+types/layout";
+import { appRoutes } from "../../routes";
+import { User } from "../../models/User";
+import { userContext } from "../../context";
+
+/*
+  Client-side authentication middleware.
+
+  API resources are protected, so its reasonable to do this just
+  for user convenience of not loading things they won't need
+*/
+const authMiddleware: Route.ClientMiddlewareFunction = async ({ context }) => {
+  const token = localStorage.getItem("user");
+  if (!token) {
+    console.log("Missing session token! Rerouting to authenticate...")
+    throw redirect(appRoutes.auth.login)
+  }
+
+  try {
+    const user: User = JSON.parse(token)
+    context.set(userContext, user);
+  } catch {
+    console.error("Invalid session token! Rerouting to reauthenticate...")
+
+    localStorage.removeItem("user")
+    throw redirect(appRoutes.auth.login)
+  }
+}
+
+export const clientMiddleware = [authMiddleware];
+
 
 export default function Layout() {
   return (
