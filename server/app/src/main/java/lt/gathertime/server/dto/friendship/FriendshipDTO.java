ff5df0f6306110d39489c5acc643821dc540ae68
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-12-09 22:23:10 +0200

    fix: use FriendshipDTO

---
+++ server/app/src/main/java/lt/gathertime/server/dto/friendship/FriendshipDTO.java
@@ -0,0 +1,21 @@
+package lt.gathertime.server.dto.friendship;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class FriendshipDTO {
+    
+    private Long friendshipId;
+
+    private Long friendId;
+
+    private String firstName;
+
+    private String lastName;
+}
