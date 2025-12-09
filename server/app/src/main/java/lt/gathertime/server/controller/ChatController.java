1c25a71669333f6ec1f134d23f87b3520d4c3861
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-12-09 20:16:16 +0200

    feat: edit comment

--- server/app/src/main/java/lt/gathertime/server/controller/ChatController.java
+++ server/app/src/main/java/lt/gathertime/server/controller/ChatController.java
@@ -2,17 +2,20 @@ package lt.gathertime.server.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lt.gathertime.server.dto.comment.CommentDTO;
-import lt.gathertime.server.dto.comment.CreateCommentRequestDTO;
+import lt.gathertime.server.dto.comment.UpsertCommentRequestDTO;
 import lt.gathertime.server.service.ChatService;
 
 @RestController
@@ -23,15 +26,28 @@ public class ChatController {
     private final ChatService chatService;
 
     @PostMapping("/user/{userId}/comment")
+    @ResponseStatus(HttpStatus.CREATED)
     public CommentDTO createComment(
         @PathVariable Long chatId,
         @PathVariable Long userId,
-        @Valid @RequestBody CreateCommentRequestDTO payload) {
+        @Valid @RequestBody UpsertCommentRequestDTO payload) {
             return chatService.createComment(chatId, userId, payload.getContent());
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<CommentDTO> getChatComments(@PathVariable Long chatId) {
         return chatService.getChatComments(chatId);
+    }
+
+    @PutMapping("/user/{userId}/comment/{commentId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void editComment(
+        @PathVariable Long chatId,
+        @PathVariable Long userId,
+        @PathVariable Long commentId,
+        @Valid @RequestBody UpsertCommentRequestDTO payload
+    ) {
+        chatService.updateComment(chatId, userId, commentId, payload.getContent());
     }
 }
--- server/app/src/main/java/lt/gathertime/server/service/ChatService.java
+++ server/app/src/main/java/lt/gathertime/server/service/ChatService.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
@@ -13,7 +14,6 @@ import lt.gathertime.server.entity.Chat;
 import lt.gathertime.server.entity.Comment;
 import lt.gathertime.server.entity.User;
 import lt.gathertime.server.mapper.CommentMapper;
-import lt.gathertime.server.mapper.FreeTimeMapper;
 import lt.gathertime.server.repository.ChatRepository;
 import lt.gathertime.server.repository.CommentRepository;
 import lt.gathertime.server.repository.UserRepository;
@@ -58,5 +58,23 @@ public class ChatService {
                 return comments.stream()
                         .map(CommentMapper::toCommentDTO)
                         .toList();
+        }
+
+        @Transactional
+        public void updateComment(Long chatId, Long userId, Long commentId, String content) {
+                Chat chat = chatRepository.findById(chatId)
+                                .orElseThrow(() -> new RuntimeException("Chat not found with id " + chatId));
+
+                User user = userRepository.findById(userId)
+                                .orElseThrow(() -> new RuntimeException("User not found with id " + userId));
+
+                Comment comment = commentRepository.findById(commentId)
+                                .orElseThrow(() -> new RuntimeException("Comment not found with id " + commentId));
+
+                if(user.getId() != comment.getUser().getId()) {
+                        throw new AccessDeniedException("You cannot edit someone else's comment");
+                }
+
+                comment.setContent(content);
         }
 }
