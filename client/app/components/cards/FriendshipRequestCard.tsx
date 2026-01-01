bc390eb6bd19c9644d40a10f5990cebd58f31dbf
Kšyštof Tužik <krzysztof2003t@gmail.com>
2026-01-01 23:41:19 +0200

    formatting

--- client/app/components/cards/FriendshipRequestCard.tsx
+++ client/app/components/cards/FriendshipRequestCard.tsx
@@ -42,9 +42,11 @@ const FriendshipRequestCard: React.FC<FriendshipRequestCardProps> = ({
           onClick={handleAccept}
           disabled={loading}
           className={`px-4 py-2 rounded-xl transition 
-            ${loading 
-              ? "bg-gray-400 text-gray-200 cursor-not-allowed" 
-              : "bg-blue-500 text-white hover:bg-blue-600"}`}
+            ${
+              loading 
+                ? "bg-gray-400 text-gray-200 cursor-not-allowed" 
+                : "bg-blue-500 text-white hover:bg-blue-600"
+            }`}
         >
           Patvirtinti
         </button>
